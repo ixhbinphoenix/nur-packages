9798399f8db7f3066559858a3542fd9f3af00702
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2023-08-24 22:05:04 +0200

    what-the-dog-doin: Hopefully fix build

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,20 @@
-{ pkgs }:
-
+{ pkgs, fetchFromGithub, lib }:
 with pkgs.lib; {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+  mk-rust-platform = { date, channel }:
+    let
+      mozilla-overlay = fetchFromGitHub {
+        owner = "mozilla";
+        repo = "nixpkgs-mozilla";
+        rev = "db89c8707edcffefcd8e738459d511543a339ff5";
+        sha256 = lib.fakeSha256;
+      };
+      mozilla = pkgs.callPackage "${mozilla-overlay.out}/package-set.nix" {};
+      rustSpecific = (mozilla.rustChannelOf { inherit date channel ; }).rust;
+    in pkgs.makeRustPlatform {
+      cargo = rustSpecific;
+      rustc = rustSpecific;
+    };
 }
